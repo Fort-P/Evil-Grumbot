9e0c5a71762fdb71b334f98be246b2985de328ad
Gideon McKinlay <gmckinlay04@gmail.com>
2025-03-16 02:18:42 +1000

    Major refactor

--- evil_grumbot.py
+++ evil_grumbot.py
@@ -1,35 +1,37 @@
-import os
-from typing import Any, Callable, Literal
 import logging
+import os
 import socket
+from typing import Any, Literal
 
 import discord
-from discord import app_commands, utils
-
+from discord import app_commands
 from mcstatus import JavaServer
 
-from custom_logger import Logger, BasicFormatter
+from custom_logger import Logger
+
+# Initialise a custom logger
 logger = Logger('evil.grumbot')
 
-
-class ServerDoesNotSupportQuerying(Exception):
-    pass
+MAX_RETRIES = 5
 
 
 class MyBot(discord.Client):
-    def __init__(self, *args: Any, **kwargs: Any):
+    """Custom Discord bot client."""
+
+    def __init__(self, **kwargs: Any):
         discord.VoiceClient.warn_nacl = False
 
         intents = discord.Intents.default()
-        super().__init__(*args,
-                         intents=intents,
-                         command_prefix="!",
-                         activity=discord.Game(name='Gwaff'),
-                         **kwargs)
+        super().__init__(intents=intents, command_prefix="!",
+                         activity=discord.Game(name='Gwaff'), **kwargs)
 
         self.synced = False
 
     async def on_ready(self) -> None:
+        """
+        Event handler for when the bot is ready.
+        Syncs commands with the Discord server.
+        """
         if not self.synced:
             await tree.sync()
             for server in self.guilds:
@@ -38,113 +40,203 @@ class MyBot(discord.Client):
             self.synced = True
         logging.info("Ready!")
 
-    async def on_app_command_completion(self, interaction, command):
+    async def on_app_command_completion(self, interaction: discord.Interaction,
+                                        command: app_commands.Command) -> None:
+        """
+        Event handler for when an application command is completed.
+        Logs the command usage.
+        """
         logger.info(f"User '{interaction.user.name}' "
                     f"used command '{command.name}' "
                     f"in guild '{interaction.guild.name}'")
 
 
+# Initialize the bot and command tree
 bot = MyBot()
 tree = app_commands.CommandTree(bot)
 
-survival_channels: list[int] = [930585547842404372, 930322945040072734]
-events_channels: list[int] = [1241016401502928967, 1237006920863453225, 1167958292245512213, 1276552236323045437]
-creative_channels: list[int] = []
-testing_channels: list[int] = [646113723550924849]
 
-server_type = Literal["Default", "Survival", "Events", "Creative"]
+class Server:
+    """
+    Class representing a Minecraft server.
+    """
 
-ips: dict[server_type, str] = {
-    "Survival": "173.233.142.94:25565",
-    "Events": "173.233.142.10:25565",
-    "Creative": "173.233.142.2:25565",
-    "Testing": "173.233.142.3:25565"
-}
+    def __init__(self, name: str, ip: str, channels: list[int], secret: bool = False,
+                 supports_querying: bool = True):
+        self.name = name
+        self.ip = ip
+        self.channels = channels
+        self.secret = secret
+        self.supports_querying = supports_querying
 
 
-@tree.command(name="list",
-              description="Lists the active members of a spooncraft server.")
-@app_commands.describe(server='The Minecraft server to check. Not required in certain channels.',
-                        hidden='Hide from others in this server (Default True). May depend on the server.')
+servers = [Server("Survival", "173.233.142.94:25565", [930585547842404372, 930322945040072734],
+                  supports_querying=False),
+           Server("Events", "173.233.142.10:25565", [1241016401502928967, 1237006920863453225],
+                  supports_querying=False),
+           Server("Creative", "173.233.142.2:25565", [930324293840171028], supports_querying=False),
+           Server("Testing", "173.233.142.3:25565", [646113723550924849], True,
+                  supports_querying=False),
+           Server("Events Building", "140.238.96.87:25565", [], True, supports_querying=False)]
+
+server_type = Literal[*[s.name for s in servers]]
+server_options = Literal["Default", *[s.name for s in servers if not s.secret]]
+
+
+def get_server_from_channel(channel_id: int) -> Server:
+    """
+    Get the server name based on the channel ID.
+
+    Args:
+        channel_id (int): The channel ID.
+
+    Returns:
+        str: The server name.
+    """
+    for s in servers:
+        if channel_id in s.channels:
+            return s
+    raise ValueError(f"Channel ID '{channel_id}' not found.")
+
+
+def get_server(server: server_type) -> Server:
+    """
+    Get the server IP address based on the server name.
+
+    Args:
+        server (server_type): The server name.
+
+    Returns:
+        str: The server IP address.
+    """
+    for s in servers:
+        if s.name == server:
+            return s
+    raise ValueError(f"Server '{server}' not found.")
+
+
+def get_server_info(server: Server):
+    """
+    Get the server information of the given server.
+
+    Args:
+        server (server_type): The server name.
+
+    Returns:
+        The server information.
+    """
+    server_lookup = JavaServer.lookup(server.ip)
+
+    count = 0
+    while count < MAX_RETRIES:
+        try:
+            return server_lookup.status()
+        except socket.timeout:
+            logging.warning(f"Status TimeoutError {count}")
+            count += 1
+        except Exception as e:
+            logging.warning(f"Status Unknown exception {e} {count}")
+            count += 1
+
+
+def get_players_query(server: Server) -> list:
+    """
+    Get the server query information of the given server.
+
+    Args:
+        server (server_type): The server name.
+    """
+    server_lookup = JavaServer.lookup(server.ip)
+    query = server_lookup.query()
+    return query.players.names
+
+
+def compose_response(server, status, player_list) -> str:
+    """
+    Compose the response message for the server status.
+
+    Args:
+        server (Server): The server object.
+        status: The server status response.
+        player_list (list): The list of players.
+    """
+    server_name = f"**Spooncraft {server.name} Server**\n"
+    player_count = status.players.online
+    max_count = status.players.max
+    if player_count == 0:
+        return f"{server_name}**No online players**"
+
+    if len(player_list) == 0 and player_count >= 1:
+        player_list.append('Anonymous Player')
+    elif len(player_list) < player_count:
+        player_list.append('...')
+    players_str = ', '.join(player_list)
+
+    return f"{server_name}**Online players ({player_count}/{max_count}):**\n```{players_str}```"
+
+
+@tree.command(name="list", description="Lists the active members of a spooncraft server.")
+@app_commands.describe(server='Optional. The Minecraft server to check.',
+                       hidden='Optional. Hide from others in this server.')
 @app_commands.allowed_installs(guilds=True, users=True)
-async def send_data(interaction: discord.Interaction,
-                    server: server_type = "Default",
+async def send_data(interaction: discord.Interaction, server: server_options = "Default",
                     hidden: bool = True):
+    """
+    Command to list active members of a specified Minecraft server.
+
+    Args:
+        interaction (discord.Interaction): The interaction object.
+        server (server_type): The Minecraft server to check.
+        hidden (bool): Whether to hide the response from others.
+    """
     await interaction.response.defer(ephemeral=hidden)
+
+    selected_server: Server
     if server == "Default":
-        if interaction.channel_id in survival_channels:
-            server = "Survival"
-        elif interaction.channel_id in events_channels:
-            server = "Events"
-        elif interaction.channel_id in creative_channels:
-            server = "Creative"
-        elif interaction.channel_id in testing_channels:
-            server = "Testing"
-        else:
+        try:
+            selected_server = get_server_from_channel(interaction.channel_id)
+        except ValueError:
             await interaction.followup.send("**You must either select a server "
                                             "or be in a recognised channel**")
             return
-    server_name = f"**Spooncraft {server} Server**\n"
-    server_lookup = JavaServer.lookup(ips.get(server))
+    else:
+        selected_server = get_server(server)
 
     # Attempt to get the server information.
-    status: JavaServer.JavaStatusResponse = None
-    count = 0
-    while count < 5:
-        try:
-            status = server_lookup.status()
-            break
-        except socket.timeout:
-            logging.warn(f"Status TimeoutError {count}")
-            count += 1
-        except Exception as e:
-            logging.warn(f"Status Unknown exception {e} {count}")
-            count += 1
-    # Attempt an error had occured.
+    status = get_server_info(selected_server)
+
+    # Attempt an error had occurred.
     if status is None:
         await interaction.followup.send(f"**An unexpected error occurred**")
         return
 
-    player_count = status.players.online
-    if player_count == 0:
-        await interaction.followup.send(f"{server_name}**No online players**")
+    if status.players.online == 0:
+        await interaction.followup.send(compose_response(selected_server, status, []))
         return
-
-    max_count = status.players.max
 
     # Attempt to get the player list
-    try:
-        # Try through query if server supports it
-        if server in ("Events", "Testing"):
-            raise ServerDoesNotSupportQuerying(
-                f"{server} Server does not support queying method."
-            )
-        query = server_lookup.query()
-        players = query.players.names
-        player_list = ', '.join(players)
-        await interaction.followup.send(f"{server_name}**Online players ({player_count}/{max_count}):**\n```{player_list}```")
-        return
-    except (socket.timeout, ServerDoesNotSupportQuerying) as e:
-        if e is socket.timeout:
-            logging.error(f'Using backup for {server}, despite being supported.')
-        else:
-            logging.warning('Using backup')
-        players = status.players.sample
-        player_list = ', '.join(
-            [i.name for i in players if i.name != "Anonymous Player"])
-        # Sample has a possibility of missing players.
-        if len(player_list) == 0 and player_count >= 1:
-            player_list += 'Anonymous Player'
-        elif len(players) < player_count:
-            player_list += ', ...'
-        await interaction.followup.send(f"{server_name}**Online players ({player_count}/{max_count}):**\n```{player_list}```")
-        return
+    player_list = []
+    if selected_server.supports_querying:
+        try:
+            player_list = get_players_query(selected_server)
+            print(player_list)
+        except socket.timeout:
+            logging.error(f"Using backup for '{selected_server.name}', despite being supported.")
+    if len(player_list) == 0:
+        player_list = [i.name for i in status.players.sample if i.name != "Anonymous Player"]
+    print(player_list)
+
+    await interaction.followup.send(compose_response(selected_server, status, player_list))
+    return
 
 
-def runTheBot(token) -> None:
-    '''
-    Runs the bot
-    '''
+def run_the_bot(token) -> None:
+    """
+    Runs the bot with the provided token.
+
+    Args:
+        token (str): The bot token.
+    """
     bot.run(token)
 
 
@@ -153,7 +245,7 @@ if __name__ == '__main__':
                         format='%(levelname)8s [%(asctime)s] %(filename)13s | %(message)s',
                         datefmt='%H:%M:%S')
 
-    TOKEN = os.environ['BOT_TOKEN']
-    runTheBot(TOKEN)
+    TOKEN = os.environ.get('BOT_TOKEN')
+    run_the_bot(TOKEN)
 
     logging.info("Fin!")
